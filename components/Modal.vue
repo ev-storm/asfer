9a435872008c40903471104280f512229c5e04d0
ev-storm <ev.git@ouylook.com>
2025-06-22 22:44:33 +0300

    v1.2

--- components/Modal.vue
+++ components/Modal.vue
@@ -10,19 +10,54 @@ const props = defineProps({
 });
 
 const { x, y } = useMouse();
-const xModal = ref(0);
-const updateXModal = () => {
-  xModal.value =
-    typeof window !== "undefined" ? window.innerWidth - 450 : xModal.value;
+
+const modalRef = ref(null);
+
+const CURSOR_SIZE = 200;
+
+const cursorPos = ref({ left: -1000, top: -1000 });
+
+const updateCursorPos = () => {
+  if (!props.isVisible || !modalRef.value) {
+    cursorPos.value = { left: -1000, top: -1000 };
+    return;
+  }
+
+  const rect = modalRef.value.getBoundingClientRect();
+
+  let mouseX = x.value;
+  let mouseY = y.value;
+
+  const minX = rect.left + CURSOR_SIZE / 2;
+  const maxX = rect.left + rect.width - CURSOR_SIZE / 2;
+  const minY = rect.top + CURSOR_SIZE / 2;
+  const maxY = rect.top + rect.height - CURSOR_SIZE / 2;
+
+  mouseX = Math.max(minX, Math.min(maxX, mouseX));
+  mouseY = Math.max(minY, Math.min(maxY, mouseY));
+
+  cursorPos.value = {
+    left: mouseX - CURSOR_SIZE / 2 + "px",
+    top: mouseY - CURSOR_SIZE / 2 + "px",
+  };
 };
-watch([x, y], () => {
-  updateXModal();
-});
+watch([x, y], updateCursorPos);
+
+watch(
+  () => props.isVisible,
+  (val) => {
+    if (val) {
+      nextTick(updateCursorPos);
+    } else {
+      cursorPos.value = { left: -1000, top: -1000 };
+    }
+  }
+);
 </script>
 
 <template>
-  <div class="modal-con" v-show="isVisible">
-    <div class="modal">
+  <transition name="fade-modal">
+    <div class="modal" ref="modalRef" v-show="isVisible">
       <div class="modal_title">
         <nuxt-img src="/svg/logo-mini.svg" alt="logo-modal" />
         <h2 class="actent-font">
@@ -31,15 +66,10 @@ watch([x, y], () => {
         </h2>
       </div>
     </div>
-    <div
-      class="cursor-modal"
-      :style="{
-        left: x + 'px',
-        top: y - 180 + 'px',
-        transform: 'translateX(-' + xModal + 'px)',
-      }"
-    ></div>
-  </div>
+  </transition>
+  <transition name="fade-cursor">
+    <div class="cursor-modal" :style="cursorPos" v-show="isVisible"></div>
+  </transition>
 </template>
 
 <style scoped lang="scss">
